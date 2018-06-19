324893f712e5ab044984ab97aa7d90a2a0c6f3f2
BNFrance <40396798+BNFrance@users.noreply.github.com>
2018-06-19 15:48:43 +0100

    Update elliottoolstampermonkey.js

--- elliottoolstampermonkey.js
+++ elliottoolstampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Elliottools
 // @namespace    http://*.blastingnews.com
-// @version      2.0
+// @version      2.1
 // @description  Elliottools buttons
 // @author       Elliotto
 // @match        *://*.blastingnews.com/*/*/*/*.html*
